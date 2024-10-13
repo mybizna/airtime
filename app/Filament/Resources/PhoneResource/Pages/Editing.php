a35ad1a7dce2f847021d9ebea8957f53d5ef01c2
Dedan <dedanirungu@gmail.com>
2024-10-13 16:26:35 +0300

    Release 2024.13

---
+++ app/Filament/Resources/PhoneResource/Pages/Editing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Airtime\Filament\Resources\PhoneResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\EditingBase;
+
+// Class List that extends ListBase
+class Editing extends EditingBase
+{
+}
---
+++ app/Filament/Resources/ProviderResource/Pages/Creating.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Airtime\Filament\Resources\ProviderResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\CreatingBase;
+
+// Class List that extends ListBase
+class Creating extends CreatingBase
+{
+}
